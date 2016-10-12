876f6bc9a6ea42e1860ca8ced75da782ca02ec41
Sebastian Grobelny <sgrobelny@Sebastians-MacBook-Air-2.local>
2016-10-12 03:15:27 -0700

    updating static url

--- restPokemon/settings.py
+++ restPokemon/settings.py
@@ -139,6 +139,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+# initial static
+# STATIC_URL = '/static/'
+
+STATIC_URL = ‘/static/’
+STATIC_ROOT = ‘staticfiles’
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, ‘static’),
+)
 
 SECURE_PROXY_SSL_HEADER = (‘HTTP_X_FORWARDED_PROTO’, ‘https’)
